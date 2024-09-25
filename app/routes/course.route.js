31f6df49b3274c09c02a8655af1720e203aac0fb
Nam Chu Minh <chuminhnamma@gmail.com>
2024-09-25 19:00:29 +0700

    them middelware auth cho route

--- app/routes/course.route.js
+++ app/routes/course.route.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const courseService = require('../services/course.service');
+const { authenticateToken, requireAdmin } = require('../middlewares/auth.middleware.js');
 
-router.post('/', courseService.create);
-router.delete('/:id', courseService.destroy);
-router.put('/:id', courseService.update);
+router.post('/', authenticateToken, requireAdmin, courseService.create);
+router.delete('/:id', authenticateToken, requireAdmin, courseService.destroy);
+router.put('/:id', authenticateToken, requireAdmin, courseService.update);
 router.get('/:id', courseService.show);
 router.get('/', courseService.index);
 
--- app/routes/lecturer.route.js
+++ app/routes/lecturer.route.js
@@ -2,12 +2,13 @@ const express = require('express');
 const router = express.Router();
 const lecturerService = require('../services/lecturer.service.js');
 const upload = require('../middlewares/upload.middleware.js')('uploads/lecturer_photos');
+const { authenticateToken, requireAdmin } = require('../middlewares/auth.middleware.js');
 
 
-router.delete('/:id', lecturerService.destroy);
+router.delete('/:id', authenticateToken, requireAdmin, lecturerService.destroy);
+router.put('/:id', authenticateToken, requireAdmin, upload.single('photo'), lecturerService.update);
+router.post('/', authenticateToken, requireAdmin, upload.single('photo'), lecturerService.create);
 router.get('/:id', lecturerService.show);
-router.put('/:id', upload.single('photo'), lecturerService.update);
-router.post('/', upload.single('photo'), lecturerService.create);
 router.get('/', lecturerService.index);
 
 module.exports = router;
