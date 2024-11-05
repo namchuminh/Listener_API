a80d1eb28f296069f6597f81df6bde81e8419dea
Nam Chu Minh <chuminhnamma@gmail.com>
2024-11-05 21:21:41 +0700

    cap nhat file sql

--- quanly_thinhgiang.sql
+++ quanly_thinhgiang.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 25, 2024 at 11:38 AM
+-- Generation Time: Nov 05, 2024 at 03:16 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.0.28
 
@@ -41,8 +41,13 @@ CREATE TABLE `accounts` (
 --
 
 INSERT INTO `accounts` (`account_id`, `username`, `password`, `role`, `createdAt`, `updatedAt`) VALUES
-(4, 'nguyenvana1', '$2a$10$XVu.vl8kWN7fQgaWxV/UjeC4wLqYbCBX82I0RFtnGQboSaDdNYLNC', 'lecturer', '2024-09-25 09:15:49', '2024-09-25 09:15:49'),
-(6, 'nguyenvanb', '$2a$10$xDqhzB4QQiId1B4pX7ykjOtkeOrYiD7uAOlGx7K8Ul.DtEYTVm2pm', 'lecturer', '2024-09-25 09:27:19', '2024-09-25 09:27:19');
+(4, 'nguyenvana', '$2a$10$XVu.vl8kWN7fQgaWxV/UjeC4wLqYbCBX82I0RFtnGQboSaDdNYLNC', 'admin', '2024-09-25 09:15:49', '2024-09-25 09:15:49'),
+(7, 'nguyenvanb', '$2a$10$Wx4IJJkDL2criLIk2lwEJuJCSgZrSMm4PP3dVVW63D9qe1a3WAjkG', 'lecturer', '2024-11-04 08:09:42', '2024-11-04 08:09:42'),
+(8, 'nguyenvanc', '$2a$10$bdoU7QjOJJkjiOA5YJa9Bu87mSv9DZNNzaMe7JJui1FXXNI7fzLjG', 'lecturer', '2024-11-05 09:36:52', '2024-11-05 09:36:52'),
+(9, 'g', '$2a$10$ROoPedSF4iuIPHpLDE5OEOQ.Gqdz4YYWNB5r8gnG65lqVhm80YmwW', 'lecturer', '2024-11-05 09:41:38', '2024-11-05 09:41:38'),
+(11, 'ggio', '$2a$10$/i4cEcMh/W8/HHGxALpFbuIMkuYXCJg/nQkLbY7jCuRfjO7viUWa.', 'lecturer', '2024-11-05 10:00:35', '2024-11-05 10:00:35'),
+(12, 'ggiof', '$2a$10$f68A2I2qjKtJ1HjayxqSt.w7p4kBy.HeSYs1xL7xhQO9wmzqDXmAC', 'lecturer', '2024-11-05 10:01:38', '2024-11-05 10:01:38'),
+(13, 'nam2222', '$2a$10$SZvZhLWFsLcXB8iN02WfvOsVqRGndeq3BUuBfGZ6vfH715Dhj2pGm', 'lecturer', '2024-11-05 12:45:40', '2024-11-05 12:45:40');
 
 -- --------------------------------------------------------
 
@@ -66,7 +71,8 @@ CREATE TABLE `courses` (
 
 INSERT INTO `courses` (`course_id`, `course_name`, `course_code`, `department_id`, `credits`, `createdAt`, `updatedAt`) VALUES
 (4, 'Lập trình mạng', 'LTM2024', 2, 3, '2024-09-25 08:28:45', '2024-09-25 08:28:45'),
-(5, 'Nhập môn Python', 'Python2024', 2, 3, '2024-09-25 08:29:03', '2024-09-25 08:29:03');
+(5, 'Nhập môn Python', 'Python2024', 2, 3, '2024-09-25 08:29:03', '2024-09-25 08:29:03'),
+(7, 'Lap trinh c', 'Cpp', 4, 3, '2024-11-04 09:37:22', '2024-11-04 09:37:22');
 
 -- --------------------------------------------------------
 
@@ -87,7 +93,8 @@ CREATE TABLE `departments` (
 --
 
 INSERT INTO `departments` (`department_id`, `name`, `description`, `createdAt`, `updatedAt`) VALUES
-(2, 'Phòng CNTT', 'Phòng ban công nghệ thông tin', '2024-09-25 08:09:28', '2024-09-25 08:09:28');
+(2, 'Phòng CNTT', 'Phòng ban công nghệ thông tin', '2024-09-25 08:09:28', '2024-09-25 08:09:28'),
+(4, 'Mang may tinh', 'Mang may tinh', '2024-11-04 08:22:00', '2024-11-04 08:22:00');
 
 -- --------------------------------------------------------
 
@@ -119,14 +126,11 @@ CREATE TABLE `lecturers` (
   `email` varchar(255) NOT NULL,
   `phone` varchar(20) DEFAULT NULL,
   `address` text DEFAULT NULL,
-  `degree` enum('Cử Nhân','Thạc Sĩ','Tiến Sĩ') DEFAULT 'Cử Nhân',
+  `degree` enum('Cử Nhân','Kỹ Sư','Chuyên Gia','Thạc Sĩ','Tiến Sĩ','Giáo Sư') DEFAULT 'Cử Nhân',
   `major` varchar(255) DEFAULT NULL,
-  `university` varchar(255) DEFAULT NULL,
-  `years_of_experience` int(11) DEFAULT NULL,
-  `current_position` varchar(255) DEFAULT NULL,
-  `institution` varchar(255) DEFAULT NULL,
-  `bio` text DEFAULT NULL,
   `photo_url` varchar(255) DEFAULT NULL,
+  `photo_degree` text NOT NULL,
+  `status` int(11) NOT NULL DEFAULT 0,
   `account_id` int(11) NOT NULL,
   `createdAt` timestamp NOT NULL DEFAULT current_timestamp(),
   `updatedAt` timestamp NOT NULL DEFAULT current_timestamp()
@@ -136,9 +140,14 @@ CREATE TABLE `lecturers` (
 -- Dumping data for table `lecturers`
 --
 
-INSERT INTO `lecturers` (`lecturer_id`, `name`, `gender`, `date_of_birth`, `email`, `phone`, `address`, `degree`, `major`, `university`, `years_of_experience`, `current_position`, `institution`, `bio`, `photo_url`, `account_id`, `createdAt`, `updatedAt`) VALUES
-(4, 'Nguyễn Văn An', 'Nam', '1988-05-06', 'nguyenvana1@gmail.com', '0379962047', 'Tây Hồ, Hà Nội', 'Thạc Sĩ', 'Công Nghệ Thông Tin', 'Đại học Bách Khoa Hà Nội', 3, 'Lập trình viên', 'Công ty TNHH Beeup', 'Người hướng ngoại yêu thể thao và công nghệ', 'uploads\\photo-1727255749071-150914376.jpg', 4, '2024-09-25 09:15:49', '2024-09-25 09:15:49'),
-(6, 'Nguyễn Văn Bình', 'Nam', '1988-05-06', 'nguyenvanb@gmail.com', '0379962048', 'Tây Hồ, Hà Nội', 'Thạc Sĩ', 'Công Nghệ Thông Tin', 'Đại học Bách Khoa Hà Nội', 3, 'Lập trình viên', 'Công ty TNHH Beeup', 'Người hướng ngoại yêu thể thao và công nghệ', 'uploads/lecturer_photos/photo-1727256439279-609098978.jpg', 6, '2024-09-25 09:27:19', '2024-09-25 09:27:19');
+INSERT INTO `lecturers` (`lecturer_id`, `name`, `gender`, `date_of_birth`, `email`, `phone`, `address`, `degree`, `major`, `photo_url`, `photo_degree`, `status`, `account_id`, `createdAt`, `updatedAt`) VALUES
+(4, 'Nguyễn Văn An', 'Nam', '1988-05-06', 'nguyenvana1@gmail.com', '0379962047', 'Tây Hồ, Hà Nội', 'Thạc Sĩ', 'Công Nghệ Thông Tin', 'uploads/lecturer_photos/photo-1730800834979-217482108.jpg', 'uploads/lecturer_photos/photo-1730799698019-171474197.jpg', 1, 4, '2024-09-25 09:15:49', '2024-09-25 09:15:49'),
+(7, 'Nguyễn Văn Bình', 'Nam', '1988-05-06', 'nguyenvanb@gmail.com', '0379962048', 'Tây Hồ, Hà Nội', 'Thạc Sĩ', 'Công Nghệ Thông Tin', 'uploads/lecturer_photos/photo-1730800834979-217482108.jpg', 'uploads/lecturer_photos/photo-1730799698019-171474197.jpg', 1, 7, '2024-11-04 08:09:42', '2024-11-04 08:09:42'),
+(8, 'Nguyễn Văn chung', 'Nam', '1988-05-06', 'nguyenvanc@gmail.com', '0379962042', 'Tây Hồ, Hà Nội', 'Thạc Sĩ', 'Công Nghệ Thông Tin', 'uploads/lecturer_photos/photo-1730800834979-217482108.jpg', 'uploads/lecturer_photos/photo-1730799698019-171474197.jpg', 0, 8, '2024-11-05 09:36:52', '2024-11-05 09:36:52'),
+(9, 'Nam', 'Nam', '1999-09-09', 'G@gmail.com', '099966633', 'Hn', 'Kỹ Sư', '4', 'uploads/lecturer_photos/photo-1730799698019-171474197.jpg', 'uploads/lecturer_photos/photo-1730799698019-171474197.jpg', 0, 9, '2024-11-05 09:41:38', '2024-11-05 09:41:38'),
+(11, 'Nam', 'Nam', '1999-09-09', 'GgfG@gmail.com', '099966635', 'Hn', 'Kỹ Sư', '4', 'uploads/lecturer_photos/photo-1730800834979-217482108.jpg', 'uploads/lecturer_photos/photoDegree-1730800835039-102530991.jpg', 0, 11, '2024-11-05 10:00:35', '2024-11-05 10:00:35'),
+(12, 'Nam', 'Nam', '1999-09-09', 'GgffG@gmail.com', '099966699', 'Hn', 'Kỹ Sư', '4', 'uploads/lecturer_photos/photo-1730800834979-217482108.jpg', 'uploads/lecturer_photos/photoDegree-1730800897810-621750943.jpg', 0, 12, '2024-11-05 10:01:38', '2024-11-05 10:01:38'),
+(13, 'Nam222', 'Nam', '1988-04-05', 'nam222@gmail.com', '0379962045', 'Hn', 'Thạc Sĩ', 'Mang may tinh', 'uploads/lecturer_photos/photo-1730810740492-650063149.jpg', 'uploads/lecturer_photos/photoDegree-1730810740555-160511130.jpg', 1, 13, '2024-11-05 12:45:40', '2024-11-05 12:45:40');
 
 -- --------------------------------------------------------
 
@@ -164,12 +173,26 @@ CREATE TABLE `lecturer_schedule` (
   `schedule_id` int(11) NOT NULL,
   `lecturer_id` int(11) DEFAULT NULL,
   `course_id` int(11) DEFAULT NULL,
-  `semester` varchar(50) DEFAULT NULL,
-  `start_date` date DEFAULT NULL,
-  `end_date` date DEFAULT NULL,
+  `date` date DEFAULT current_timestamp(),
+  `section` varchar(255) DEFAULT NULL,
   `status` enum('pending','approved','rejected') DEFAULT 'pending',
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+  `createdAt` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updatedAt` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `lecturer_schedule`
+--
+
+INSERT INTO `lecturer_schedule` (`schedule_id`, `lecturer_id`, `course_id`, `date`, `section`, `status`, `createdAt`, `updatedAt`) VALUES
+(2, 13, 5, '2024-11-06', 'Tiết 10 - Tiết 12', 'pending', '2024-11-05 12:56:11', '2024-11-05 12:56:11'),
+(3, 13, 7, '2024-11-10', 'Tiết 7 - Tiết 12', 'pending', '2024-11-05 13:33:26', '2024-11-05 13:33:26'),
+(4, 7, 7, '2024-11-08', 'Tiết 7 - Tiết 12', 'pending', '2024-11-05 13:35:13', '2024-11-05 13:35:13'),
+(5, 4, 7, '2024-11-08', 'Tiết 7 - Tiết 11', 'pending', '2024-11-05 13:35:45', '2024-11-05 13:35:45'),
+(6, 4, 7, '2024-11-08', 'Tiết 7 - Tiết 9', 'pending', '2024-11-05 13:38:12', '2024-11-05 13:38:12'),
+(7, 13, 7, '2024-11-08', 'Tiết 4 - Tiết 6', 'pending', '2024-11-05 13:41:55', '2024-11-05 13:41:55'),
+(8, 13, 5, '2024-11-08', 'Tiết 7 - Tiết 12', 'pending', '2024-11-05 13:43:06', '2024-11-05 13:43:06'),
+(9, 13, 5, '2024-11-08', 'Tiết 4 - Tiết 6', 'pending', '2024-11-05 13:43:33', '2024-11-05 13:43:33');
 
 -- --------------------------------------------------------
 
@@ -269,7 +292,8 @@ ALTER TABLE `lecturer_recommendations`
 ALTER TABLE `lecturer_schedule`
   ADD PRIMARY KEY (`schedule_id`),
   ADD KEY `lecturer_id` (`lecturer_id`),
-  ADD KEY `course_id` (`course_id`);
+  ADD KEY `course_id` (`course_id`),
+  ADD KEY `section_id` (`section`);
 
 --
 -- Indexes for table `reports`
@@ -300,19 +324,19 @@ ALTER TABLE `violations`
 -- AUTO_INCREMENT for table `accounts`
 --
 ALTER TABLE `accounts`
-  MODIFY `account_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `account_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `courses`
 --
 ALTER TABLE `courses`
-  MODIFY `course_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `course_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `departments`
 --
 ALTER TABLE `departments`
-  MODIFY `department_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `department_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `department_requests`
@@ -324,7 +348,7 @@ ALTER TABLE `department_requests`
 -- AUTO_INCREMENT for table `lecturers`
 --
 ALTER TABLE `lecturers`
-  MODIFY `lecturer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `lecturer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `lecturer_recommendations`
@@ -336,7 +360,7 @@ ALTER TABLE `lecturer_recommendations`
 -- AUTO_INCREMENT for table `lecturer_schedule`
 --
 ALTER TABLE `lecturer_schedule`
-  MODIFY `schedule_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `schedule_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT for table `reports`
